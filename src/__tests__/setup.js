dc9ab40dfbde6aecb66883c6295df65928eb8679
JxAmani <jeremyamani336@gmail.com>
2025-07-29 14:40:44 +0300

    Update

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,9 +1,10 @@
+// src/__tests__/setup.js
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
-import 'whatwg-fetch';
-import {Blob} from 'node:buffer';
+import 'whatwg-fetch'
+import { Blob } from 'node:buffer'
 
 afterEach(() => {
-    cleanup();
+  cleanup()
 })
